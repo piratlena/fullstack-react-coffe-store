33a360e3fbc5d67bc1ea3997080fed93b677bf29
Elena Brazhnikova <lenovosibirsk@mail.ru>
2023-01-06 12:33:54 +0700

    feat: add middleware that checks if user is authorized or not, and user is admin or not

--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,11 +1,12 @@
 const Router = require('express')
 const router = new Router()
 const typeController = require('../controllers/typeController')
+const checkRole = require('../middleware/checkMiddleWare')
 
 
 
 
-router.post('/', typeController.create )
+router.post('/', checkRole("ADMIN"), typeController.create )
 router.get('/', typeController.getAll)
 
 
